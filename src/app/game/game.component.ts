678c00cc5a1dfc3190d55cdb3a31e06baa6c0e3b
Mario Haefs <MarioHaefs@t-online.de>
2023-07-26 18:44:01 +0200

    added Players & shuffle Cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() { };
+
+  ngOnInit(): void {
+    this.newGame();
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+      console.log('New Card: ' + this.currentCard);
+      console.log('Game is', this.game);
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000)
+    }
+  }
+
+  newGame() {
+    this.game = new Game();
   }
 
 }
