9c50604a8e2f870fe2b4704c855f9f36563eb312
Mario Haefs <MarioHaefs@t-online.de>
2023-08-02 12:53:26 +0200

    start to synchronize firebase game for multiplayer

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,8 +2,9 @@ import { Component, inject } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, addDoc, doc, getDoc, getDocs, query, where, limit } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -13,16 +14,46 @@ import { Observable } from 'rxjs';
 
 export class GameComponent {
   firestore: Firestore = inject(Firestore);
+  games$: Observable<any[]>;
+  games: Array<any>;
   pickCardAnimation = false;
   currentCard: string = '';
   game!: Game;
 
 
-  constructor(public dialog: MatDialog) { };
+  constructor(public dialog: MatDialog, private route: ActivatedRoute) {
+    this.newGame();
+  
+    this.route.params.subscribe((params) => {
+      const gameId = params['id'];
+      console.log(gameId);
+  
+      const gamesCollection = collection(this.firestore, 'games');
+      const gameDoc = doc(gamesCollection, gameId);
+  
+      getDoc(gameDoc)
+        .then((gameSnapshot) => {
+          const currentGame = gameSnapshot.data() as Game;
+  
+          currentGame.currentPlayer = this.game.currentPlayer;
+          currentGame.playedCards = this.game.playedCards;
+          currentGame.players = this.game.players;
+          currentGame.stack = this.game.stack;
+  
+          console.log('Dieses Spiel', currentGame);
+          console.log('Spieler', currentGame.players);
+        })
+    });
+  
+    this.games$ = collectionData(collection(this.firestore, 'games'));
+    this.games$.subscribe((newGame: any) => {
+      this.games = newGame;
+    });
+  }
+  
 
 
   ngOnInit(): void {
-    this.newGame();
   }
 
 
@@ -43,6 +74,9 @@ export class GameComponent {
 
   newGame() {
     this.game = new Game();
+    console.log('In der Funktion newGame', this.game)
+    // let addNewGameToFirebase = collection(this.firestore, 'games');
+    // addDoc(addNewGameToFirebase, this.game.toJson());
   }
 
 
